04f1e6c66c2c202f727ef30598e8a660cabfcca8
Imoh Precious <jaypee88830@gmail.com>
2023-07-03 00:27:55 +0100

    page setup

---
+++ src/components/CryptoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CryptoDetails = () => {
+	return(
+		<>
+			<h1>Homepage</h1>
+		</>
+	)
+}
+
+export default CryptoDetails
---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cryptocurrencies = () => {
+	return(
+		<>
+
+		</>
+	)
+}
+
+export default Cryptocurrencies
